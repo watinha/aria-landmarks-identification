6cda11aa43d43f1c5e150500845c1df48bd7bda8
Willian Massami Watanabe <watinha@gmail.com>
2023-04-28 18:56:28 -0300

    saving trained classifiers

--- experiment.py
+++ experiment.py
@@ -1,4 +1,4 @@
-import pandas as pd, numpy as np, sys
+import pandas as pd, numpy as np, sys, pickle
 
 from sklearn.model_selection import GroupShuffleSplit
 
@@ -15,10 +15,11 @@ groups = df['base_url'].to_numpy()
 X = np.abs(df[base_columns].to_numpy() - df[target_columns].to_numpy())
 y = df['similar'].to_numpy()
 
-y_pred = []
-y_true = []
+classifiers = ['dt', 'rf', 'lsvm', 'knn']
+results = {}
+for classifier in classifiers:
+  results[classifier] = { 'y_true': [], 'y_pred': [] }
 
-classifier_name = 'rf'
 _, ncol = X.shape
 
 folds = GroupShuffleSplit(n_splits=10, random_state=42)
@@ -27,10 +28,27 @@ for train, test in folds.split(X, y, groups):
   X_test, y_test = X[test, :], y[test]
   group_train = groups[train]
 
-  pipeline = get_pipeline(classifier_name, ncol)
-  pipeline.fit(X_train, y_train, groups=group_train)
+  for classifier in classifiers:
+    pipeline = get_pipeline(classifier, ncol)
+    pipeline.fit(X_train, y_train, groups=group_train)
 
-  y_pred.extend(pipeline.predict(X_test))
-  y_true.extend(y_test)
+    results[classifier]['y_pred'].extend(pipeline.predict(X_test))
+    results[classifier]['y_true'].extend(y_test)
 
-print(metrics.classification_report(y_true, y_pred))
+
+for classifier in classifiers:
+  y_pred = results[classifier]['y_pred']
+  y_true = results[classifier]['y_true']
+  print('==========================')
+  print('-                        -')
+  print('-   %s    -' % (classifier))
+  print()
+  print(metrics.classification_report(y_true, y_pred))
+  print()
+
+  pipeline = get_pipeline(classifier, ncol)
+  pipeline.fit(X_train, y_train, groups=groups)
+
+  with open('./results/classifier/pipeline-similarity-%s.sav' % (classifier), 'wb') as f:
+    pickle.dump(pipeline, f)
+
